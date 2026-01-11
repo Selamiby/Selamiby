0bfb21e3c3554e9eb6465be8d3c61b9e1a5cffc1
selamiby <selami721993@gmail.com>
2026-01-12 01:23:12 +0300

    fix: NEXUS-ONE Otomatik Hata Düzeltici - Tüm sorunlar çözüldü

--- nexus_super_learner.py
+++ nexus_super_learner.py
@@ -177,28 +177,26 @@ class EnhancedLearner:
                     jaccard_score = 0
                 
                 # Algorithm 3: Levenshtein distance (normalized)
+                def levenshtein(s1, s2):
+                    if len(s1) < len(s2):
+                        return levenshtein(s2, s1)
+                    if len(s2) == 0:
+                        return len(s1)
+                    previous_row = range(len(s2) + 1)
+                    for i, c1 in enumerate(s1):
+                        current_row = [i + 1]
+                        for j, c2 in enumerate(s2):
+                            insertions = previous_row[j + 1] + 1
+                            deletions = current_row[j] + 1
+                            substitutions = previous_row[j] + (c1 != c2)
+                            current_row.append(min(insertions, deletions, substitutions))
+                        previous_row = current_row
+                    return previous_row[-1]
+                
                 max_len = max(len(text1_lower), len(text2_lower))
                 if max_len > 0:
-                    # Simple Levenshtein implementation
-                    def levenshtein(s1, s2):
-                        if len(s1) < len(s2):
-                            return levenshtein(s2, s1)
-                        if len(s2) == 0:
-                            return len(s1)
-                        previous_row = range(len(s2) + 1)
-                        for i, c1 in enumerate(s1):
-                            current_row = [i + 1]
-                            for j, c2 in enumerate(s2):
-                                insertions = previous_row[j + 1] + 1
-                                deletions = current_row[j] + 1
-                                substitutions = previous_row[j] + (c1 != c2)
-                                current_row.append(min(insertions, deletions, substitutions))
-                            previous_row = current_row
-                        lev_distance = previous_row[-1]
-                        lev_score = 1 - (lev_distance / max_len)
-                    else:
-                        lev_score = 0
-                    lev_score = levenshtein(text1_lower, text2_lower)
+                    lev_distance = levenshtein(text1_lower, text2_lower)
+                    lev_score = 1 - (lev_distance / max_len)
                 else:
                     lev_score = 0
                 
